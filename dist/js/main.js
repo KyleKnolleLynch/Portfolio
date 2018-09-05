226a9026f535baab23a6600532e62339db9bb551
Kyle Lynch <kylelynch104@gmail.com>
2018-09-05 00:49:54 -0700

    add_service_worker

--- dist/js/main.js
+++ dist/js/main.js
@@ -34,7 +34,7 @@ function toggleMenu() {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../../sw_cached_site.js')
+      .register('../sw_cached_site.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err} `));
   });
